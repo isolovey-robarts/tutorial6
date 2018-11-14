8317802a731361179a769eb48f241f800f8ff9e4
Igor Solovey <isolovey@robarts.ca>
2018-11-14 11:34:02 -0500

    Added documentation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def function1():
+    """simple function"""
     return 5
 
 def function2(x):
+    """square a number"""
     return x*x
 
 def function5(x,y,z):
+    """multiply and subtract"""
     return x*y*z-3
 
 def main():
+    "print linear combination"
     print function1() - function2(5) + function5(1,2,3)
 
