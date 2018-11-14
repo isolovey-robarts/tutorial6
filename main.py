707fb52ec31213e9ced6e24f5535c5b4b1fb336b
Igor Solovey <isolovey@robarts.ca>
2018-11-14 11:32:39 -0500

    Added function5

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def function1():
 def function2(x):
     return x*x
 
+def function5(x,y,z):
+    return x*y*z-3
 
 def main():
-    print function1() - function2(5)
+    print function1() - function2(5) + function5(1,2,3)
 
