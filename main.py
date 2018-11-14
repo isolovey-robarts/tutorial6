999b33ae7977168da81a8a7c318935ccccf68637
Igor Solovey <isolovey@robarts.ca>
2018-11-14 11:31:29 -0500

    Added a complicated function4

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def function1():
 def function2(x):
     return x*x
 
+def function3(x, y):
+    return x-y*y+5
+
 
 def main():
-    print function1() - function2(5)
+    print function1() - function2(5) + function3(6,7)
 
